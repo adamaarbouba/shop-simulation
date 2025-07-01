3df9ee4c21fe91f06dd51aea473d95042480af25
Adam Arbouba <adamaarbouba@gmail.com>
2025-07-01 13:58:04 +0100

    modualated code

--- Shop_Simulation/input.c
+++ Shop_Simulation/input.c
@@ -2,14 +2,8 @@
 #include <stdio.h>
 #include <string.h>
 #include <ctype.h>
+#include "utils.h"
 
-int strcasecmp_custom(const char *s1, const char *s2) {
-    while (*s1 && *s2) {
-        if (tolower((unsigned char)*s1) != tolower((unsigned char)*s2)) return 1;
-        s1++; s2++;
-    }
-    return *s1 || *s2;
-}
 
 int safe_int_input(const char *prompt, int max) {
     char line[64];
